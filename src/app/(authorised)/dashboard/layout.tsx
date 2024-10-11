33f8e975b2882a2c5cd569cfa2a8d62a1cdfd0c9
Ryan O'Hara <ryan.a.ohara@gmail.com>
2024-10-11 16:23:59 +1300

    fix: make sure users with no account go through onboarding

--- src/app/(authorised)/dashboard/layout.tsx
+++ src/app/(authorised)/dashboard/layout.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Sidebar } from "@/components/sidebar";
 import { getUser } from "@/lib/supabase/queries/user.cached";
+import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const user = await getUser();
+
+	if (!user) {
+		redirect("/login");
+	}
+
+	if (!user.data?.account_id) {
+		redirect("/onboarding");
+	}
+
 	return (
 		<div className="flex h-screen">
 			<Sidebar />
