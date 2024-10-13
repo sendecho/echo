194a560f5bc36f1da2da1b37ec678e91e1bf6830
Ryan O'Hara <ryan.a.ohara@gmail.com>
2024-10-14 09:27:11 +1300

    fix: add plain text email

--- src/emails/index.tsx
+++ src/emails/index.tsx
@@ -1,25 +1,40 @@
-import { JSXElementConstructor, ReactElement } from "react";
+import type { JSXElementConstructor, ReactElement } from "react";
 import { Resend } from "resend";
+import { render } from "@react-email/render";
 import { env } from "@/env";
 
-const resend = new Resend(env.RESEND_API_KEY || '');
+const resend = new Resend(env.RESEND_API_KEY || "");
 
-const SEND_TO_TEST_EMAIL = env.USE_TEST_EMAIL === true || (env.NODE_ENV === 'development' && env.USE_TEST_EMAIL !== false);
+const SEND_TO_TEST_EMAIL =
+	env.USE_TEST_EMAIL === true ||
+	(env.NODE_ENV === "development" && env.USE_TEST_EMAIL !== false);
 
 interface SendEmailProps {
-  from?: string;
-  email: string;
-  subject: string;
-  react: ReactElement<any, string | JSXElementConstructor<any>>;
+	from?: string;
+	email: string;
+	subject: string;
+	react: ReactElement<any, string | JSXElementConstructor<any>>;
 }
 
-export const sendEmail = async ({ from = "Echo <no-reply@sendecho.co>", email, subject, react }: SendEmailProps) => {
-  const toEmail = SEND_TO_TEST_EMAIL ? "delivered@resend.dev" : email;
+const plainText = async (
+	react: ReactElement<any, string | JSXElementConstructor<any>>,
+) => {
+	return render(react, { plainText: true });
+};
 
-  return await resend.emails.send({
-    from: from,
-    to: [toEmail],
-    subject: subject,
-    react: react,
-  });
+export const sendEmail = async ({
+	from = "Echo <no-reply@sendecho.co>",
+	email,
+	subject,
+	react,
+}: SendEmailProps) => {
+	const toEmail = SEND_TO_TEST_EMAIL ? "delivered@resend.dev" : email;
+
+	return await resend.emails.send({
+		from: from,
+		to: [toEmail],
+		subject: subject,
+		react: react,
+		text: await plainText(react),
+	});
 };
